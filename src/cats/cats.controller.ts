e7f7fe46ab19b463b74bc2354fdfa453c790dedc
Vincenzo Bianco <vincenzo.bianco@optilyz.com>
2020-03-30 10:23:17 +0200

    add guard

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,14 +1,24 @@
-import { Controller, Get, Post, Body, HttpException, HttpStatus, UseFilters, UsePipes } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  UseFilters,
+  UseGuards,
+  UsePipes,
+} from '@nestjs/common';
 import { ApiTags, ApiCreatedResponse } from '@nestjs/swagger';
 import { Cat } from './interfaces/cat.interface';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { CatsService } from './cats.service';
 import { HttpExceptionFilter } from '../common/exceptions/http-exception.filer';
 import { JoiValidationPipe } from '../common/pipes/joi.validation.pipe';
+import { AuthGuard } from '../common/guards/auth.guard';
 import { createCatSchema } from './cats.schema.validations'
 
 @ApiTags('cats')
 @Controller('cats')
+@UseGuards(AuthGuard)
 @UseFilters(HttpExceptionFilter)
 export class CatsController {
   constructor(private catsService: CatsService) {}
